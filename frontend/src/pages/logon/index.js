1805cb79372fe503f63bee6eac941ced761c3c44
Rafael Arthur Rocha Miranda <43150739+rafael-miranda10@users.noreply.github.com>
2020-03-26 18:25:50 -0300

    [DEV] front-end da aplicação

--- frontend/src/pages/logon/index.js
+++ frontend/src/pages/logon/index.js
@@ -1,23 +1,50 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { FiLogIn } from 'react-icons/fi';
 import './styles.css';
 import herosImg from '../../assets/heroes.png';
 import logoImg from '../../assets/logo.svg';
+import { Link, useHistory } from 'react-router-dom';
+import api from '../../services/api';
 
 
 export default function Logon() {
+
+   const [id, setId] = useState('');
+
+   const history = useHistory();
+
+  async function handleLogin(e){
+    e.preventDefault();
+    const data = {id};
+
+    try{
+        const response = await api.post('/sessions',data);
+        localStorage.setItem('ongId',id);
+        localStorage.setItem('ongName', response.data.name);
+        history.push('/profile');
+    }catch(error){
+        setId('');
+        alert('Erro no login, tente novamente');
+    }
+  }
+
+
     return (
         <div className="logon-container">
             <section className="form">
                 <img src={logoImg} alt="Be The Hero" />
-                <form>
+                <form onSubmit={handleLogin}>
                     <h1>Faça seu logon</h1>
-                    <input placeholder="Sua ID" />
+                    <input
+                     placeholder="Sua ID" 
+                     value={id}
+                     onChange={e => setId(e.target.value)}
+                     />
                     <button type="submit" className="button" >Entrar</button>
-                    <a href="/register">
-                    <FiLogIn size={16} color="#E02041" />
+                    <Link className="back-link" to="/register">
+                        <FiLogIn size={16} color="#E02041" />
                     Não tenho cadastro
-                    </a>
+                    </Link>
                 </form>
 
             </section>
